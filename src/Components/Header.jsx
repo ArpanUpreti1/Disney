26c788b29ea622621715022a7636a763635faa9b
ArpanUpreti1 <arpanuprety50@gmail.com>
2025-04-15 13:16:14 +0545

    Fixed navigation and Production house

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,86 +1,104 @@
-import React, { useState } from 'react'
-import logo from '../assets/Images/disney.png'
-import { 
-    HiHome,
-    HiMagnifyingGlass,
-    HiStar,
-    HiPlayCircle,
-    HiTv,
-    HiPlus,
-    HiMiniEllipsisVertical
-} from "react-icons/hi2";
+import React, { useState, useRef, useEffect } from 'react';
+import logo from '../assets/Images/disney.png';
+import {
+  HiHome,
+  HiMagnifyingGlass,
+  HiStar,
+  HiPlayCircle,
+  HiTv,
+  HiPlus,
+  HiMiniEllipsisVertical,
+} from 'react-icons/hi2';
 
 import HeaderItem from './HeaderItem';
 
 const Header = () => {
-  const [toogle, setToogle] = useState(false);
-    const menu = [
-        {
-            name:'HOME',
-            icon:HiHome
-        },
-        {
-            name:'SEARCH',
-            icon:HiMagnifyingGlass
-        },
-        {
-            name:'WATCH LIST',
-            icon:HiPlus
-        },
-        {
-            name:'ORIGNALS',
-            icon:HiStar
-        },
-        {
-            name:'MOVIES',
-            icon:HiPlayCircle
-        },
-        {
-            name:'SERIES',
-            icon:HiTv
-        }
-    ];
+  const [toggle, setToggle] = useState(false);
+  const dropdownRef = useRef(null);
 
-    return (
-        <div className='flex items-center justify-between p-5 md:p-5'>
-            <div className='flex items-center gap-8 md:gap-[400px]'>
-                <img src={logo} className='w-[60px] md:w-[130px] object-cover' alt="Logo" />
-                
-                
-                <div className='hidden md:flex gap-[4vw] items-center'>
-                    {menu.map((item, index) => (
-                        <HeaderItem key={index} name={item.name} Icon={item.icon} />
-                    ))}
-                </div>
-                
-          
-                <div className='flex md:hidden gap-8 items-center'>
-                    {menu.slice(0, 3).map((item, index) => (
-                        <HeaderItem key={index} name={''} Icon={item.icon} />
-                      
-                    ))}
-                </div>
-                <div className='md:hidden' onClick={()=>setToogle(!toogle)}>
-                    <HeaderItem name={''} Icon={HiMiniEllipsisVertical} />
-                    {toogle?
-                    <div className='absolute mt-3 bg-[#f7f7f7] p-3 '>
-                    {menu.slice(3, 6).map((item, index) => (
-                        <HeaderItem key={index} name={item.name} Icon={item.icon} />
-                      
-                    ))}
-                    </div>:null}
-                </div>
-            </div>
-                <img 
-                    src="https://www.pngarts.com/files/5/Cartoon-Avatar-Transparent-Image.png" 
-                    className='w-[30px] h-[30px] md:w-[40px] md:h-[40px] rounded-full bg-yellow-300' 
-                    alt="Avatar" 
-                />
-            
-                
-       
+  // Close dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setToggle(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const menu = [
+    { name: 'HOME', icon: HiHome },
+    { name: 'SEARCH', icon: HiMagnifyingGlass },
+    { name: 'WATCH LIST', icon: HiPlus },
+    { name: 'ORIGINALS', icon: HiStar },
+    { name: 'MOVIES', icon: HiPlayCircle },
+    { name: 'SERIES', icon: HiTv },
+  ];
+
+  return (
+    <header className="flex items-center justify-between p-4 md:p-6">
+      {/* Logo */}
+      <div className="flex items-center">
+        <img
+          src={logo}
+          className="w-[50px] md:w-[120px] object-cover"
+          alt="Disney Logo"
+        />
+      </div>
+
+      {/* Desktop Menu */}
+      <nav className="hidden md:flex gap-8">
+        {menu.map((item, index) => (
+          <HeaderItem key={index} name={item.name} Icon={item.icon} />
+        ))}
+      </nav>
+
+      {/* Mobile Menu */}
+      <div className="flex md:hidden items-center gap-4 relative">
+        {/* First 3 Menu Items */}
+        <div className="flex gap-4">
+          {menu.slice(0, 3).map((item, index) => (
+            <HeaderItem key={index} name="" Icon={item.icon} />
+          ))}
         </div>
-    );
+
+        {/* Toggle Button */}
+        <div ref={dropdownRef} className="relative">
+          <button
+            className="flex items-center"
+            onClick={() => setToggle(!toggle)}
+            aria-label="Toggle Menu"
+          >
+            <HeaderItem name="" Icon={HiMiniEllipsisVertical} />
+          </button>
+
+          {/* Dropdown Menu */}
+          {toggle && (
+            <div className="absolute top-10 right-0 bg-[#121212] border border-gray-700 shadow-xl rounded-lg p-4 min-w-[160px] z-50">
+              {menu.slice(3).map((item, index) => (
+                <div key={index} className="py-2 hover:bg-gray-800 rounded px-2">
+                  <HeaderItem name={item.name} Icon={item.icon} />
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+
+      {/* User Avatar */}
+      <div>
+        <img
+          src="https://www.pngarts.com/files/5/Cartoon-Avatar-Transparent-Image.png"
+          className="w-[30px] h-[30px] md:w-[40px] md:h-[40px] rounded-full bg-yellow-300"
+          alt="User Avatar"
+        />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/Components/ProductionHouse.jsx
+++ src/Components/ProductionHouse.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import disney from '../assets/Images/disney.png'
 import marvel from '../assets/Images/marvel.png'
 import pixar from '../assets/Images/pixar.png'
@@ -15,37 +15,51 @@ import starwarsV from '../assets/Videoes/star-wars.mp4'
 const ProductionHouse = () => {
     const list = [
         {
-            id:1,
-            image:disney,
-            video:disneyV
+            id: 1,
+            image: disney,
+            video: disneyV
         },
         {
-            id:2,
-            image:marvel,
-            video:marvelV
+            id: 2,
+            image: marvel,
+            video: marvelV
         },
         {
-            id:3,
-            image:pixar,
-            video:pixarV
+            id: 3,
+            image: pixar,
+            video: pixarV
         },
         {
-            id:4,
-            image:nationalG,
-            video:nationalGV
+            id: 4,
+            image: nationalG,
+            video: nationalGV
         },
         {
-            id:5,
-            image:starwars,
-            video:starwarsV
+            id: 5,
+            image: starwars,
+            video: starwarsV
         }
     ]
   return (
-    <div className='flex gap-5 p-2 px-5 md:px-16 '>
+    <div className='flex gap-5 p-2 px-5 md:px-16'>
       {list.map((item) => (
-        <div className='border-[2px] border-gray-600 rounded-lg hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer shadow-xl shadow-black'>
-           <img src={item.image} alt="" srcset="" className='w-full z-[1]'/> 
-           <video src={item.video} autoPlay loop playsInline className='absolute top-0 rounded-md z-0 opacity-0 hover:opacity-50'></video>
+        <div 
+          key={item.id} 
+          className='relative border-[2px] border-gray-600 rounded-lg hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer shadow-xl shadow-black overflow-hidden group'
+        >
+           <img 
+             src={item.image} 
+             alt={`${item.id} logo`} 
+             className='w-full relative z-10'
+           /> 
+           <video 
+             src={item.video} 
+             autoPlay 
+             loop 
+             playsInline 
+             muted
+             className='absolute top-0 left-0 w-full h-full object-cover z-0 opacity-0 group-hover:opacity-50 transition-opacity duration-300'
+           />
         </div>
       ))}
     </div>
